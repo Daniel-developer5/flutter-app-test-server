faa96d8b45c116d8a2a6f791e2d26770d17cb352
Daniel-developer5 <daniel.mudrak2005@gmail.com>
2023-07-06 10:27:22 +0200

    init

---
+++ index.js
@@ -0,0 +1,20 @@
+const express = require('express')
+const app = express()
+const PORT = 3000 || process.env.PORT
+
+app.use(require('cors')())
+app.use(express.json())
+
+app.get('/', (_, res) => {
+  res.send('Flutter Prawnik Test Server')
+})
+
+app.post('/message', (req, res) => {
+  const { body } = req
+
+  console.log(body)
+
+  res.send({ status: 200 })
+})
+
+app.listen(PORT)
