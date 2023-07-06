fd86b3f0300b45ffeee1c4045b67873564c42e84
Daniel-developer5 <daniel.mudrak2005@gmail.com>
2023-07-06 12:03:49 +0200

    add api function

---
+++ functions/hello-world/api.js
@@ -0,0 +1,25 @@
+const serverless = require('serverless-http')
+const express = require('express')
+const app = express()
+
+const router = express.Router()
+
+app.use(require('cors')())
+app.use(express.json())
+
+router.get('/', (_, res) => {
+  res.send('Flutter Prawnik Test Server')
+})
+
+app.post('/message', (req, res) => {
+  const { body } = req
+
+  console.log(body)
+
+  res.send({ status: 200 })
+})
+
+// app.use(`/.netlify/functions/api`, router)
+
+const handler = serverless(app)
+module.exports = { handler }
